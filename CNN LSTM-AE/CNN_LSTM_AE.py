d1387b1b377556383de872ae61a031b465164cb7
jorrelbrandonz <jorrelzamora@gmail.com>
2022-05-18 16:23:41 +0800

    Added training and output functions

--- CNN LSTM-AE/CNN_LSTM_AE.py
+++ CNN LSTM-AE/CNN_LSTM_AE.py
@@ -1,17 +1,30 @@
 from types import DynamicClassAttribute
+from pandas.io import parsers
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Data preprocessing - Formats the data file to a usable format for the model
 def prepareData(trainFile, step):
     trainData = pd.read_csv(trainFile, parse_dates=['Time']).drop(['Type'], axis=1)
-    print(trainData.describe())
     trainData = trainData.values
-    print(trainData)
-        
 
-def createModel():
+    feature = []
+    label = []
+    for i in range(len(trainData)):
+        endidx = i + step
+        if endidx > len(trainData) - 1:
+            break
+        seq_x, seq_y = trainData[1:endidx, ], trainData[endidx, 0]
+        feature.append(seq_x)
+        label.append(seq_y)
+
+    feature, label = np.array(feature), np.array(label)
+    return feature, label
+
+#Model creation - Assembles and compiles the whole network model to be used
+def createModel(feature, label):
     #CNN LSTM model creation
     model = tf.keras.Sequential()
     model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, )))
@@ -20,10 +33,22 @@ def createModel():
     model.add(Dense(1))
     model.compile(loss='mse', optimizer=adam)
     print("Model created!")
+    return model
 
-def startTrain():
-    print("Model trained!")
+#Training - Fits the model into the data and begins the training
+def startTrain(model, train_feature, train_label, validation_feature, validation_label):
+    dataInfo = model.fit(train_feature, train_label, epochs=30, batch_size=10,
+                         validation_data=(validation_feature, validation_label))
+    metric = open(metricFile, 'a')
+    for i in range(len(metricInfo.history['loss'])):
+        metric.write('{}, {}, {}, {}, {}\n'.format(i + 1, dataInfo.history['loss'][i],
+                                                   dataInfo.history['val_loss'][i],
+                                                   dataInfo.history['root_mean_squared_error'][i],
+                                                   dataInfo.history['val_root_mean_squared_error'][i]))
+    metric.close()
+    return model
 
+#Function that contains all the process to train the model
 def trainModel():
     dataDir = ".//"
     trainFile = ["KITCHEN_data_record.csv", "LIVINGROOM_data_record.csv", 
@@ -37,7 +62,10 @@ def trainModel():
     nSteps = [5, 7, 9]
     for i in range(len(trainFile)):
         for step in nSteps:
-            prepareData(dataDir + trainFile[i], step)
+            trainX, trainY = prepareData(dataDir + trainFile[i], step)
+            model = createModel(trainX.shape)
+
+            print(trainX)
  
     #createModel()
     #startTrain()
