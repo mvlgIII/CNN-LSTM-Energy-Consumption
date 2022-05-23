e02e2ffcb14b9e503553b23a4e2921fbdda1aa8f
jorrelbrandonz <jorrelzamora@gmail.com>
2022-05-23 08:58:44 +0800

    Edited data files utilized in code

--- CNN LSTM-AE/CNN_LSTM_AE.py
+++ CNN LSTM-AE/CNN_LSTM_AE.py
@@ -2,7 +2,6 @@ from types import DynamicClassAttribute
 from pandas.io import parsers
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 #Data preprocessing - Formats the data file to a usable format for the model
@@ -13,22 +12,23 @@ def prepareData(trainFile, step):
     feature = []
     label = []
     for i in range(len(trainData)):
-        endidx = i + step
-        if endidx > len(trainData) - 1:
+        end = i + step
+        if end > len(trainData) - 1:
             break
-        seq_x, seq_y = trainData[1:endidx, ], trainData[endidx, 0]
+        seq_x, seq_y = trainData[i:end, ], trainData[end, 1:]
         feature.append(seq_x)
         label.append(seq_y)
 
     feature, label = np.array(feature), np.array(label)
+    print("Shape of feature: {}".format(feature.shape))
     return feature, label
 
 #Model creation - Assembles and compiles the whole network model to be used
-def createModel(feature, label):
+def createModel(shape):
     #CNN LSTM model creation
     model = tf.keras.Sequential()
-    model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, )))
-    model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+    model.add(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(shape))
+    model.add(MaxPooling1D(pool_size=2))
     model.add(LSTM(50, activation='relu'))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer=adam)
@@ -54,18 +54,19 @@ def trainModel():
     trainFile = ["KITCHEN_data_record.csv", "LIVINGROOM_data_record.csv", 
                  "CENTRALIZED_data_record.csv"]
 
-    testFile = ["KITCHEN_data_record.csv", "LIVINGROOM_data_record.csv", 
-                 "CENTRALIZED_data_record.csv"]
+    testFile = ["KITCHEN_test.csv", "LIVINGROOM_test.csv", 
+                 "CENTRALIZED_test.csv"]
 
-    outputFile = ["KITCHEN_result", "LIVINGROOM_result"]
+    outputFile = ["KITCHEN_result", "LIVINGROOM_result", "CENTRALIZED_result"]
 
     nSteps = [5, 7, 9]
     for i in range(len(trainFile)):
         for step in nSteps:
             trainX, trainY = prepareData(dataDir + trainFile[i], step)
-            model = createModel(trainX.shape)
+            #print(trainX.shape)
+            #model = createModel(trainX.shape)
 
-            print(trainX)
+           #print(trainX)
  
     #createModel()
     #startTrain()
