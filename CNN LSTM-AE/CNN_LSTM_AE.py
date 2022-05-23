1eef0363573c1a9d887889d1d4dc7ecd388f1993
jorrelbrandonz <jorrelzamora@gmail.com>
2022-05-23 14:13:51 +0800

    Training data and test data separation, preprocessing data update

--- CNN LSTM-AE/CNN_LSTM_AE.py
+++ CNN LSTM-AE/CNN_LSTM_AE.py
@@ -1,12 +1,19 @@
-from types import DynamicClassAttribute
-from pandas.io import parsers
+from pandas.core.tools.datetimes import to_datetime
 import tensorflow as tf
+from tensorflow.keras import optimizers
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.python.keras.layers.convolutional import Conv1D, MaxPooling1D
+from tensorflow.python.keras.layers import Dense, LSTM, RepeatVector, Flatten
+
 import numpy as np
 import pandas as pd
 
 #Data preprocessing - Formats the data file to a usable format for the model
 def prepareData(trainFile, step):
     trainData = pd.read_csv(trainFile, parse_dates=['Time']).drop(['Type'], axis=1)
+    trainData['Time'] = pd.to_numeric(pd.to_datetime(trainData['Time']))
+    print(trainData['Time'])
     trainData = trainData.values
 
     feature = []
@@ -19,7 +26,7 @@ def prepareData(trainFile, step):
         feature.append(seq_x)
         label.append(seq_y)
 
-    feature, label = np.array(feature), np.array(label)
+    feature, label = np.array(feature).astype(np.float32), np.array(label).astype(np.float32)
     print("Shape of feature: {}".format(feature.shape))
     return feature, label
 
@@ -27,11 +34,11 @@ def prepareData(trainFile, step):
 def createModel(shape):
     #CNN LSTM model creation
     model = tf.keras.Sequential()
-    model.add(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(shape))
+    model.add(Conv1D(filters=128, kernel_size=2, activation='relu', input_shape=(shape)))
     model.add(MaxPooling1D(pool_size=2))
     model.add(LSTM(50, activation='relu'))
     model.add(Dense(1))
-    model.compile(loss='mse', optimizer=adam)
+    model.compile(loss='mse', optimizer='adam')
     print("Model created!")
     return model
 
@@ -62,14 +69,10 @@ def trainModel():
     nSteps = [5, 7, 9]
     for i in range(len(trainFile)):
         for step in nSteps:
-            trainX, trainY = prepareData(dataDir + trainFile[i], step)
-            #print(trainX.shape)
-            #model = createModel(trainX.shape)
-
-           #print(trainX)
- 
-    #createModel()
-    #startTrain()
+            train_feature, train_label = prepareData(dataDir + trainFile[i], step)
+            test_feature, test_label = prepareData(dataDir + testFile[i], step)
+            model = createModel(train_feature)
+            model = startTrain(model, train_feature, train_label, test_feature, test_label)
 
 if __name__ == "__main__":
     trainModel()
