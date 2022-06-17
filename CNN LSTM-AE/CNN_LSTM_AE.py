deebb4838f956e64b3f3cc4e73fc6e7a3dc715e0
jorrelbrandonz <jorrelzamora@gmail.com>
2022-06-17 11:27:42 +0800

    Preprocessing update

--- CNN LSTM-AE/CNN_LSTM_AE.py
+++ CNN LSTM-AE/CNN_LSTM_AE.py
@@ -1,24 +1,35 @@
+from pandas._libs.tslibs.timestamps import Timestamp
 from pandas.core.tools.datetimes import to_datetime
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.python.keras import optimizers, layers
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, LSTM, RepeatVector, Flatten
+from tensorflow.keras.layers import Dense, LSTM, RepeatVector, Flatten, TimeDistributed
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Reshape, Bidirectional
 from tensorflow.keras.callbacks import EarlyStopping
 
 import numpy as np
 import pandas as pd
+import datetime
+from datetime import datetime
 from sklearn import preprocessing as prc
+from sklearn.preprocessing import MinMaxScaler
 
 #Data preprocessing - Formats the data file to a usable format for the model
 def prepareData(trainFile, step):
     trainData = pd.read_csv(trainFile).drop(['Type', 'Time'], axis=1)
-    #trainData['Time'] = pd.to_numeric(pd.to_datetime(trainData['Time']))
+    #trainData['Time'] = pd.to_datetime(trainData['Time'])
+    #for i in range(len(trainData)):
+    #    trainData['Time'][i] = trainData['Time'][i].timestamp()
+    #    print(trainData['Time'][i])
     #trainNormal = prc.normalize(trainData)
     #trainData = pd.DataFrame(trainNormal)
     #print(trainData.head())
     #print(trainData.describe())
+    scaler = MinMaxScaler()
+    trainData = scaler.fit_transform(trainData)
+    trainData = pd.DataFrame(trainData)
+    print(trainData.head())
     trainData = trainData.values
 
     feature = []
@@ -30,17 +41,17 @@ def prepareData(trainFile, step):
         seq_x, seq_y = trainData[i:end, ], trainData[end, 0] * trainData[end, 1]
         feature.append(seq_x)
         label.append(seq_y)
-
+        
     feature, label = np.array(feature).astype(np.float32), np.array(label).astype(np.float32)
-    print("Shape of feature: {}".format(feature.shape))
+    #print("Shape of feature: {}".format(feature.shape))
     return feature, label
 
 #Model creation - Assembles and compiles the whole network model to be used
 def createTeacherModel(shape):
     #CNN LSTM model creation
-    tf.random.set_seed(100)
+    tf.random.set_seed(1)
     model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(shape[1], shape[2])))
+    model.add(Conv1D(filters=64, kernel_size=2, activation='sigmoid', input_shape=(shape[1], shape[2])))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Bidirectional(LSTM(300, activation='sigmoid', return_sequences=True)))
     model.add(Dense(1))
@@ -53,7 +64,7 @@ def createTeacherModel(shape):
 
 #Training - Fits the model into the data and begins the training while recording metric values
 def startTrain(model, train_feature, train_label, validation_feature, validation_label, metricFile):
-    dataInfo = model.fit(train_feature, train_label, epochs=100, batch_size=128, #batch size multiple of 2^x, early stopping00
+    dataInfo = model.fit(train_feature, train_label, epochs=2, batch_size=128, #batch size multiple of 2^x, early stopping00
                          validation_data=(validation_feature, validation_label),
                          callbacks=[EarlyStopping(monitor='loss', patience=3)])
     #metric = model.evaluate(validation_feature, validation_label)  
@@ -80,13 +91,15 @@ def trainModel():
                  "CENTRALIZED_result"]
     
     model = 0
+    predictions = []
 
-    nSteps = [7, 9]
+    nSteps = [9]
     #Centralized learning approach:
     for step in nSteps:
         print("Preparing data {}_{}".format(trainFile[2], step))
         train_feature, train_label = prepareData(dataDir + trainFile[2], step)
         test_feature, test_label = prepareData(dataDir + testFile[2], step)
+        print("Feature: {} Label: {}". format(train_feature, train_label))
         model = createTeacherModel(train_feature.shape)
         metricFile = dataDir + outputFile[2] + "_CNN-LSTM_" + str(step) + "_steps.csv"
         print("Training " + metricFile)
@@ -97,7 +110,7 @@ def trainModel():
         model.save(dataDir + outputFile[2] + '_CNN-LSTM_' + str(step) + "_steps_model")
         predictions.append(model.predict(test_feature, verbose=False))
             
-        #print(predictions)
+        print(predictions)
         #studentModel = createStudentModel(predictions.shape)
 
     #student_train_feature
